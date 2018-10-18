d5a65d85fb112dc054fe2032cb3daaa399434646
Alessandro Maggio <alex.tkd.alex@gmail.com>
2018-10-18 16:33:05 +0200

    Iterate sentece, word, check synonymous from method and database support for limit request to virgilio

--- main.py
+++ main.py
@@ -6,28 +6,45 @@ from __future__ import unicode_literals
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-import treetaggerwrapper, re, nltk
+import treetaggerwrapper, re, nltk, italian_dictionary, utils
 from pprint import pprint
+from pymongo import MongoClient
 
-# nltk.download('punkt')
+nltk.download('punkt')
 
-# treetaggerwrapper.enable_debugging_log()
+client = MongoClient()
+database = client[ 'word_history' ]
+
+propose = [ 'andare', 'fare' ]
+negation = [ 'non' ]
+
 tagger = treetaggerwrapper.TreeTagger(TAGLANG='it', TAGDIR='./TreeTagger', TAGPARFILE='./TreeTagger/lib/italian.par')
-message = 'Non ho vogl.ia di fare niente sta sera. anche se pensavo di andare al cinema. Che ne dite?'
+message = 'Non ho vogl. ia di fare niente sta sera. anche se pensavo di andare al cinema. Che ne dite?'
 sentences = nltk.sent_tokenize( message )
+
 for sentence in sentences:
+    print("Sentence: {}".format(sentence))
+
     tags_encoded = tagger.tag_text( sentence )
     tags = treetaggerwrapper.make_tags( tags_encoded )
 
-    pprint(tags)
-
-    propose = [ 'andare', 'fare' ]
-    negation = [ 'non' ]
-
     is_negate = False
     for tag in tags:
-        if tag.word in negation:
-            is_negate = True
+        print(tag)
 
-        if tag.pos.startswith("VER") and tag.lemma in propose and is_negate is False :
-            print("C'è l'intenzione di voler fare qualcosa")
+        if tag.pos.startswith(("VER", "PRO", "NPR", "NOM", "ADJ")):
+                synonymous = database.synonymous.find_one({"word": tag.word})
+                if synonymous is None:
+                        synonymous = utils.scrape_synonymous(tag.word)
+                        database.synonymous.insert_one({'word': tag.word, 'synonymous': synonymous})
+                else:
+                        synonymous = synonymous['synonymous']
+
+                print(synonymous)
+                print("==============")
+
+        # if tag.word in negation:
+        #     is_negate = True
+
+        # if tag.pos.startswith("VER") and tag.lemma in propose and is_negate is False :
+        #     print("C'è l'intenzione di voler fare qualcosa")
