c65835d42486b8cb187f5cab36d07c0ef2fd168a
Alessandro Maggio <alex.tkd.alex@gmail.com>
2018-10-24 11:44:50 +0200

    Convert chat_data into database storage

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import treetaggerwrapper, re, nltk, logging, json, utils
 from pprint import pprint
 from pymongo import MongoClient
+from bson import ObjectId
 
 # Telegram import 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
@@ -27,11 +28,11 @@ logger = logging.getLogger('MAIN')
 nltk.download('punkt')
 
 client = MongoClient()
-database = client[ 'word_analysis' ]
+database = client[ 'pool_group' ]
 
 proposes = [ 'andare', 'fare' ]
-proposes += utils.get_synonymous(database, 'andare')
-proposes += utils.get_synonymous(database, 'fare')
+# proposes += utils.get_synonymous(database, 'andare')
+# proposes += utils.get_synonymous(database, 'fare')
 
 negation = [ 'non' ]
 
@@ -50,14 +51,12 @@ preposition = {
 # Dove andiamo oggi?
 # Io pensavo di andare al cinema
 # Io voglio studiare programmazione
-def analyzes_message(bot, update, chat_data):
-    # Create chat data array sentence
-    if "sentence" not in chat_data:
-        chat_data["sentence"] = []
 
+# IO DIREI DI STUDIARE (NOT WORK)
+def analyzes_message(bot, update, chat_data):
     sentences = nltk.sent_tokenize( update.message.text.lower() )
     proposals = []
-    title_proposes = None
+    pool_title = None
     for sentence in sentences:
         tags_encoded = tagger.tag_text( sentence )
         tags = treetaggerwrapper.make_tags( tags_encoded )
@@ -68,106 +67,117 @@ def analyzes_message(bot, update, chat_data):
             'status': False
         }
         for index in range(0, len(tags)):
+            print(tags[index])
 
             if tags[index].word in negation:
                 is_negate = True
 
-            print(tags[index])
             if tags[index].pos.startswith(("VER", "NOM")) and utils.good_middle(middle, tags) is True:
                 propose = None
                 if tags[middle['index']-1].lemma != "andare":
                     propose = "{} {} {}".format(tags[middle['index']-1].word, tags[middle['index']].word, tags[index].word)
                 elif tags[index].lemma != "fare":
                     propose = tags[index].word
-
+                
                 if propose != None and propose not in proposals:
                     proposals.append(propose)
             
             if tags[index].pos.startswith("VER") and tags[index].lemma in proposes and is_negate is False:
-                # title_proposes = sentence.capitalize()
-                title_proposes = update.message.text
+                pool_title = update.message.text
 
             # Foundend preposition maybe the next word is a 'place' or 'action to do'
             # Anzichè escludere DI conviene includere solo A
             if tags[index].pos.startswith(("PRE", "DET")) and tags[index].word != 'di' and not tags[index].word in preposition['di']:
                 middle['status'] = True 
                 middle['index'] = index
-     
-    if [ a for a in chat_data["sentence"] if a['closed'] is False ] == []:
-        if title_proposes != None:
-            
+
+    last_pool = database.pool.find_one({"chat_id": update.message.chat_id, "closed": False})
+    if last_pool is None:
+        if pool_title != None:
             keyboard = [ [] ]
+            new_pool = {
+                "chat_id": update.message.chat_id,
+                "title": pool_title,
+                "closed": False,
+                "proposals": [ 
+                    {
+                        "propose": p, 
+                        "voted_by": [] 
+                    } for p in proposals ]
+            }
+            database.pool.insert_one( new_pool )
+            
             for index in range(0, len(proposals)):
                 if len(keyboard[len(keyboard)-1]) == 3:
                     keyboard.append([])
-                
-                keyboard[len(keyboard)-1].append( InlineKeyboardButton(proposals[index].capitalize(), callback_data=index) )
+
+                callback_data = json.dumps({ 'index': index, 'pool_id': str(new_pool['_id']) })   
+                keyboard[len(keyboard)-1].append( InlineKeyboardButton(proposals[index].capitalize(), callback_data=callback_data) )
     
             reply_markup = InlineKeyboardMarkup(keyboard)
-            new_sentence = bot.send_message(update.message.chat_id, title_proposes, reply_markup=reply_markup)
-            chat_data["sentence"].append({
-                    "message_id": new_sentence.message_id,
-                    "title": title_proposes,
-                    "closed": False,
-                    "proposals": [ {"propose": p, "vote": 0 } for p in proposals ]
-                }
-            )
+            new_pool_message = bot.send_message(update.message.chat_id, pool_title, reply_markup=reply_markup)
+            database.pool.update_one({"_id": new_pool['_id']}, {"$set": {"message_id": new_pool_message.message_id} })
     else:
-        # Foundend open sentence add proposals:
+        # Foundend open pool and new proposals to add:
         if proposals != []:
-            last_sentence = chat_data["sentence"][len(chat_data["sentence"])-1]
-            old_proposals = last_sentence["proposals"]
             for propose in proposals:
-                if propose not in [ p['propose'] for p in old_proposals ]:
-                    last_sentence["proposals"].append({
+                if propose not in [ p['propose'] for p in last_pool["proposals"] ]:
+                    last_pool["proposals"].append({
                         "propose": propose, 
-                        "vote": 0
+                        "voted_by": []
                     })
 
             keyboard = [ [] ]
-            proposals = last_sentence["proposals"]
-            for index in range(0, len(proposals)):
+            for index in range(0, len(last_pool["proposals"])):
                 if len(keyboard[len(keyboard)-1]) == 3:
                     keyboard.append([])
                 
+                callback_data = json.dumps({ 'index': index, 'pool_id': str(last_pool['_id']) })
                 keyboard[len(keyboard)-1].append(
                     InlineKeyboardButton( 
-                        "{} {}".format(proposals[index]["propose"].capitalize(), "" if proposals[index]["vote"] == 0 else "({})".format(proposals[index]["vote"]) 
-                    ) , callback_data=index ) 
+                        "{} {}".format(
+                            last_pool["proposals"][index]["propose"].capitalize(), 
+                            "" if len(last_pool["proposals"][index]["voted_by"]) == 0 else "({})".format(len(last_pool["proposals"][index]["voted_by"])) 
+                    ) , callback_data=callback_data ) 
                 )
             
+            database.pool.update_one({"_id": last_pool['_id']}, {"$set": {'proposals': last_pool["proposals"]} })
             reply_markup = InlineKeyboardMarkup(keyboard)
             bot.edit_message_text(
-                last_sentence['title'], 
+                last_pool['title'], 
                 chat_id=update.message.chat_id, 
-                message_id=last_sentence['message_id'], 
+                message_id=last_pool['message_id'], 
                 reply_markup=reply_markup
             )
 
 def button(bot, update, chat_data):
     query = update.callback_query
-    chat_data["sentence"][len(chat_data["sentence"])-1]['proposals'][int(query.data)]['vote'] += 1
-    
-    last_sentence = chat_data["sentence"][len(chat_data["sentence"])-1]
-    keyboard = [ [] ]
-    proposals = last_sentence["proposals"]
-    for index in range(0, len(proposals)):
-        if len(keyboard[len(keyboard)-1]) == 3:
-            keyboard.append([])
+    callback_data = json.loads(query.data)
+    pool = database.pool.find_one({"_id": ObjectId(callback_data['pool_id'])})
+    if query.from_user.id not in pool['proposals'][callback_data['index']]['voted_by']: 
+        pool['proposals'][callback_data['index']]['voted_by'].append(query.from_user.id)
+        keyboard = [ [] ]
+        for index in range(0, len(pool['proposals'])):
+            if len(keyboard[len(keyboard)-1]) == 3:
+                keyboard.append([])
+            
+            callback_data = json.dumps({ 'index': index, 'pool_id': str(pool['_id']) })
+            keyboard[len(keyboard)-1].append(
+                InlineKeyboardButton( 
+                    "{} {}".format(
+                        pool['proposals'][index]["propose"].capitalize(), 
+                        "" if len(pool['proposals'][index]["voted_by"]) == 0 else "({})".format(len(pool['proposals'][index]["voted_by"])) 
+                ) , callback_data=callback_data ) 
+            )
         
-        keyboard[len(keyboard)-1].append(
-            InlineKeyboardButton( 
-                "{} {}".format(proposals[index]["propose"].capitalize(), "" if proposals[index]["vote"] == 0 else "({})".format(proposals[index]["vote"]) 
-            ) , callback_data=index )
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        bot.edit_message_text(
+            pool['title'], 
+            chat_id=query.message.chat.id, 
+            message_id=pool['message_id'], 
+            reply_markup=reply_markup
         )
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    bot.edit_message_text(
-        last_sentence['title'], 
-        chat_id=query.message.chat.id, 
-        message_id=last_sentence['message_id'], 
-        reply_markup=reply_markup
-    )
+        database.pool.update_one({"_id": pool['_id']}, {"$set": {'proposals': pool["proposals"]} })
 
 def error(bot, update, error):
     logger.error('Update "%s" caused error "%s"' % (update, error))
