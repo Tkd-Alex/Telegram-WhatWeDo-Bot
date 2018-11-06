fac005143e048b602ec8b7185b28ba1401d42c4e
Alessandro Maggio <alex.tkd.alex@gmail.com>
2018-11-06 16:26:43 +0100

    Create file with pointer variable

---
+++ pointers.py
@@ -0,0 +1,13 @@
+day_transformers = {
+    "domani": 1,
+    "dopodomani": 2
+}
+
+time_pointers = {
+    "stasera": 20,
+    "sera": 20,
+    "pranzo": 13,
+    "cena": 21,
+    "colazione": 8,
+    "oggi": 0
+}
